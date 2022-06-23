a09be069f215b191553cba9012127d966d717595
LAPTOP <raflimaulana192@gmail.com>
2022-06-23 17:19:11 +0700

    Recyclerview APP

---
+++ app/src/main/java/com/indahkurniawati/recyclerview/model/Pemain.kt
@@ -0,0 +1,11 @@
+package com.indahkurniawati.recyclerview.model
+
+data class Pemain(
+    var name : String,
+    var foto : Int,
+    var posisi : String,
+    var tinggi : String,
+    var tempatlahir : String,
+    var tgllahir : String
+)
+
